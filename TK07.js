ec655fdc49d938ff1f3eeff0df063460d68a1984
nandhakv <108390577+nandhakv@users.noreply.github.com>
2024-04-19 21:00:24 +0530

    Update TK07.js

--- TK07.js
+++ TK07.js
@@ -1,28 +1,33 @@
-const soap = require('soap');
-
-const url = 'http://your-soap-service-url?wsdl'; // replace with your SOAP service URL
-const args = {
-  Input_Data: {
-    CLOSING_DATE: '2023-04-08',
-    CLOSING_TIME: '12:29:20',
-    PLANT_CODE: '3102',
-    TANK_NO: 'S007',
-    MATERIAL: '',
-    TANK_DIP: '9546',
-    WATER_DIP: '0',
-    DENSITY: '545',
-    MATERIAL_TEMP: '28',
-    VAPOUR_TEMP: '37',
-    VAPOUR_PRESSURE: '5.16',
-    EVENT_ID: '3102200016',
-    USERNAME: 'PIDOTECH',
-  },
+const url = 'http://your-soap-service-url'; // replace with your SOAP service URL
+const headers = {
+  'Content-Type': 'text/xml; charset=utf-8',
+  'SOAPAction': 'http://your-soap-action' // replace with your SOAPAction
 };
 
-soap.createClientAsync(url).then((client) => {
-  return client.Tank_Closing_Data_Input_MT(args);
-}).then((result) => {
-  console.log(result);
-}).catch((err) => {
-  console.error(err);
-});
+const body = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tank="http://Tank_Closing_Data_WS">
+    <soapenv:Header/>
+    <soapenv:Body>
+        <tank:Tank_Closing_Data_Input_MT>
+            <Input_Data>
+                <CLOSING_DATE>2023-04-08</CLOSING_DATE>
+                <CLOSING_TIME>12:29:20</CLOSING_TIME>
+                <PLANT_CODE>3102</PLANT_CODE>
+                <TANK_NO>S007</TANK_NO>
+                <MATERIAL> </MATERIAL>
+                <TANK_DIP>  9546</TANK_DIP>
+                <WATER_DIP>0</WATER_DIP>
+                <DENSITY>545</DENSITY>
+                <MATERIAL_TEMP>  28</MATERIAL_TEMP>
+                <VAPOUR_TEMP>  37</VAPOUR_TEMP>
+                <VAPOUR_PRESSURE>5.16</VAPOUR_PRESSURE>
+                <EVENT_ID>3102200016</EVENT_ID>
+                <USERNAME>PIDOTECH</USERNAME>
+            </Input_Data>
+        </tank:Tank_Closing_Data_Input_MT>
+    </soapenv:Body>
+</soapenv:Envelope>`;
+
+fetch(url, { method: 'POST', headers, body })
+  .then(response => response.text())
+  .then(text => console.log(text))
+  .catch(err => console.error(err));
